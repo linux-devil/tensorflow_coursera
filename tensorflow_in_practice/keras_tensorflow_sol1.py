a11464f4b49cfc20a71ba689bcabc5abfaf3f6a5
linux-devil <hrsht.sarma@gmail.com>
2019-06-27 08:56:46 +0530

    tensorflow in practice coursera

--- tensorflow_in_practice/keras_tensorflow_sol1.py
+++ tensorflow_in_practice/keras_tensorflow_sol1.py
@@ -30,7 +30,7 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 train_datagen = ImageDataGenerator(rescale=1/255)
 
 train_generator = train_datagen.flow_from_directory(
-        "/tmp/h-or-s",
+        "train_dir",
         target_size=(150, 150),
         batch_size=10,
         class_mode='binary')
